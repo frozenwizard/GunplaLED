9b972b4131e53efbbbee4272642d8da661b5fdbb
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2023-06-25 12:30:38 -0500

    initial unicorn banshee implementation.

---
+++ src/gunpla/unicorn_banshee.py
@@ -0,0 +1,26 @@
+import time
+
+from src.gunpla.BaseGundam import BaseGundam
+from src.phew.server import Request, Response
+from src.pi.led_effect import LEDEffects
+
+
+class UnicornBansheeGundam(BaseGundam):
+    """
+    RX-0 Unicorn Gundam 02 Banshee(https://gundam.fandom.com/wiki/RX-0_Unicorn_Gundam_02_Banshee)
+    """
+
+    def get_config_file(self) -> str:
+        """
+        :return: The Unicorn Banshee Gundam config file
+        """
+        return "src/config/unicorn_banshee.json"
+
+    def glow(self, request: Request) -> Response:
+        """
+        Runs the glow lightshow
+        """
+        LEDEffects.brighten_all(self.get_all_leds())
+        time.sleep(3)
+        self._all_leds_off()
+        return Response("finished", 200)
